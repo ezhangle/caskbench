6fb0cf79687893402bf5c8228dcc5e6e19d9d476
Bryce Harrington <b.harrington@samsung.com>
2014-05-07 22:08:41 -0700

    Whitespace cleanup

--- src/skia-tests/fill.cpp
+++ src/skia-tests/fill.cpp
@@ -47,30 +47,30 @@ sk_teardown_fill(void)
 {
 }
 
-void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
+void drawSkiashapes(caskbench_context_t *ctx, kinetics_t *particles)
 {
     int i, j, x, y, r, p, shape;
-    unsigned int red,green,blue,alpha;
+    unsigned int red, green, blue, alpha;
     r = 0.9 * element_spacing /2;
-    int old_x, old_y,old_width, old_height;
+    int old_x, old_y, old_width, old_height;
 
     shape = ctx->shape_args.shape_id;
-    if(!shape)
+    if (!shape)
     {
         shape = ((4.0 * rand())/RAND_MAX) + 1;
-        if(!ctx->shape_args.multi_shapes)
+        if (!ctx->shape_args.multi_shapes)
             shape = 2;
     }
 
-    if(shape)
+    if (shape)
     {
-        if(!(ctx->shape_args.center_x && ctx->shape_args.center_y))
+        if (!(ctx->shape_args.center_x && ctx->shape_args.center_y))
         {
             ctx->shape_args.center_x = x = ctx->canvas_width/2;
             ctx->shape_args.center_y = y = ctx->canvas_height/2;
         }
-        if(!ctx->shape_args.multi_shapes)
-            if(!(ctx->shape_args.width && ctx->shape_args.height))
+        if (!ctx->shape_args.multi_shapes)
+            if (!(ctx->shape_args.width && ctx->shape_args.height))
             {
                 ctx->shape_args.width = 100;
                 ctx->shape_args.height = 50;
@@ -86,16 +86,14 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
     SkPoint center;
     //Stroke styles
     SkScalar vals[] = { SkIntToScalar(1), SkIntToScalar(1)  };
-    if(ctx->shape_args.stroke_width)
+
+    if (ctx->shape_args.stroke_width)
     {
         ctx->skia_paint->setStyle(SkPaint::kStroke_Style);
-        //ctx->skia_paint->setStyle(SkPaint::kStrokeAndFill_Style);
-
         ctx->skia_paint->setStrokeWidth(ctx->shape_args.stroke_width);
-
         ctx->skia_paint->setStrokeJoin((SkPaint::Join)ctx->shape_args.join_style);
         ctx->skia_paint->setStrokeCap((SkPaint::Cap)ctx->shape_args.cap_style);
-        if(ctx->shape_args.dash_style == 0)
+        if (ctx->shape_args.dash_style == 0)
         {
 #if USE_LEGACY_SKIA_SRA
             pE.reset(new SkDashPathEffect(vals, 2, 0));
@@ -106,19 +104,11 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
 #endif
             ctx->skia_paint->setPathEffect(pE);
         }
-        else if(ctx->shape_args.dash_style == 1)
-        {
-        }
-        //                        if(ctx->shape_args.red > 0 || ctx->shape_args.blue > 0 || ctx->shape_args.green > 0 || ctx->shape_args.alpha > 0)
-        {
-            //cairo_set_source_rgba(cr,  (double)ctx->shape_args.red, (double)ctx->shape_args.green ? (double)ctx->shape_args.green:(double)0, ctx->shape_args.blue ? (double)ctx->shape_args.blue:(double)0, (double)ctx->shape_args.alpha ? (double)ctx->shape_args.alpha:(double)1);
-        }
-        //                        else
     }
 
     old_width = ctx->shape_args.width;
     old_height = ctx->shape_args.height;
-    if(!ctx->shape_args.animation && !ctx->shape_args.multi_shapes)
+    if (!ctx->shape_args.animation && !ctx->shape_args.multi_shapes)
     {
         num_x_elements = 1;
         num_y_elements = 1;
@@ -144,7 +134,7 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
             x = particles?particles->x : i * element_spacing;
 
             // Options for fill,gradient and transparency
-            if( ctx->shape_args.fill_type == NULL)
+            if (ctx->shape_args.fill_type == NULL)
             {
                 if (randomize_color)
                     skiaRandomizeColor(ctx);
@@ -154,8 +144,7 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
                 if (randomize_color)
                     skiaRandomizeColor(ctx);
             }
-            else if((strcmp(ctx->shape_args.fill_type,"herringbone-pattern")) == 0){}
-            else if((strcmp(ctx->shape_args.fill_type,"image-pattern")) == 0 &&(ctx->shape_args.image_path))
+            else if ((strcmp(ctx->shape_args.fill_type,"image-pattern")) == 0 &&(ctx->shape_args.image_path))
             {
                 SkBitmap    bm;
 
@@ -244,13 +233,13 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
                 break;
             }
 
-            if(ctx->shape_args.fill_type != NULL)
+            if (ctx->shape_args.fill_type != NULL)
             {
                 red = 255*(double)rand()/RAND_MAX;
                 green = 255*(double)rand()/RAND_MAX;
                 blue = 255*(double)rand()/RAND_MAX;
                 alpha = 255*(double)rand()/RAND_MAX;
-                if((strcmp(ctx->shape_args.fill_type,"radial-gradient")) == 0)
+                if ((strcmp(ctx->shape_args.fill_type,"radial-gradient")) == 0)
                 {
                     linearColors[1] = SkColorSetARGB (alpha, red, green, blue);
                     linearColors[0] = SkColorSetARGB (200,200,200,200);
@@ -261,7 +250,7 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
                                                              linearColors, NULL,2,
                                                              SkShader::kClamp_TileMode, NULL);
                 }
-                else if((strcmp(ctx->shape_args.fill_type,"linear-gradient")) == 0)
+                else if ((strcmp(ctx->shape_args.fill_type,"linear-gradient")) == 0)
                 {
                     linearColors[0] = SkColorSetARGB (alpha, red, green, blue);
                     linearColors[1] = SkColorSetARGB (200,200,200,200);
@@ -283,8 +272,8 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
             ctx->shape_args.height = old_height;
 
             ctx->skia_canvas->flush();
-            if(ctx->shape_args.fill_type != NULL)
-                if(shader != NULL)
+            if (ctx->shape_args.fill_type != NULL)
+                if (shader != NULL)
                 {
                     ctx->skia_paint->setShader (NULL);
                     delete shader;
@@ -293,7 +282,7 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
         }
     }
 
-    if(!ctx->shape_args.animation && !ctx->shape_args.multi_shapes)
+    if (!ctx->shape_args.animation && !ctx->shape_args.multi_shapes)
     {
         ctx->shape_args.center_x = old_x;
         ctx->shape_args.center_y = old_y;
@@ -305,28 +294,28 @@ void drawSkiashapes(caskbench_context_t *ctx,kinetics_t *particles)
 int
 sk_test_fill(caskbench_context_t *ctx)
 {
-    for(int i = 0;i < ctx->size; i++)
+    for (int i = 0;i < ctx->size; i++)
     {
-        //Animation / Kinematics of single or multi shape
-        if(ctx->shape_args.animation)
+        // Animation / Kinematics of single or multi shape
+        if (ctx->shape_args.animation)
         {
             int num_particles = ctx->shape_args.animation;
             double start_frame, stop_frame, delta;
             skia_particles = (kinetics_t *) malloc (sizeof (kinetics_t) * num_particles);
             int i,j ;
             for (i = 0; i < num_particles; i++)
-                kinetics_init (&skia_particles[i]);
+                kinetics_init(&skia_particles[i]);
             delta = 0;
 
             for (j=0;j<num_particles;j++){
-                start_frame = get_tick ();
+                start_frame = get_tick();
                 ctx->skia_canvas->drawColor(SK_ColorBLACK);
 
                 for (i = 0; i < num_particles; i++) {
                     kinetics_update(&skia_particles[i], 0.1);
-                    drawSkiashapes(ctx,&skia_particles[i]);
+                    drawSkiashapes(ctx, &skia_particles[i]);
                 }
-                stop_frame = get_tick ();
+                stop_frame = get_tick();
                 delta = stop_frame - start_frame;
             }
 
@@ -335,7 +324,7 @@ sk_test_fill(caskbench_context_t *ctx)
         // Drawing of multishape on a grid
         else
         {
-            drawSkiashapes(ctx,NULL);
+            drawSkiashapes(ctx, NULL);
         }
     }
     return 1;

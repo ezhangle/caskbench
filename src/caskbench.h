94026b7a41173090bcd71024338469bb2fcbd33d
chevalam <chevalam.s@samsung.com>
2014-05-22 15:49:50 +0530

    Issue#52Break caskbench_context stuff out to its own file

--- src/caskbench.h
+++ src/caskbench.h
@@ -23,33 +23,7 @@
 # define NUM_ELEM(x) (sizeof(x)/sizeof(x[0]))
 #endif
 
-#include "shapes.h"
-#include "forward.h"
-
-typedef struct _caskbench_context {
-    int              size;
-    int              canvas_width;
-    int              canvas_height;
-
-    cairo_t         *cairo_cr;
-    cairo_surface_t *cairo_surface;
-
-    SkPaint         *skia_paint;
-    SkCanvas        *skia_canvas;
-    SkBaseDevice    *skia_device;
-
-    cairo_surface_t *(*setup_cairo)(const device_config_t& config);
-    SkBaseDevice    *(*setup_skia)(const device_config_t& config);
-    void             (*destroy_cairo)(void);
-    void             (*destroy_skia)(void);
-    void             (*update_cairo)(void);
-    void             (*update_skia)(void);
-
-    const char      *stock_image_path;
-    shapes_t         shape_defaults;
-
-} caskbench_context_t;
-
+#include "caskbench_context.h"
 // Framework
 double get_tick (void);
 void   display_results_json(int iterations, int status);

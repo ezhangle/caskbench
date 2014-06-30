e6db65c8e94cb9882dbd989b3c3c7fac32aabbd3
suyambulingamrm <suyambu.rm@samsung.com>
2014-06-30 15:14:48 +0530

    Use utility function to create sample image.

--- src/cairo-tests/image.cpp
+++ src/cairo-tests/image.cpp
@@ -12,31 +12,14 @@
 
 #include "caskbench.h"
 #include "caskbench_context.h"
+#include <cairo-shapes.h>
 
 static cairo_surface_t *image;
 
 int
 ca_setup_image(caskbench_context_t *ctx)
 {
-    int i, x, y;
-    cairo_t *cr_image;
-
-    image = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 160, 40);
-    cr_image = cairo_create (image);
-    cairo_set_source_surface (cr_image, image, 0, 0);
-
-    x = 5;
-    for (i=0; i<16; i++) {
-        x += 10;
-        y = (i%2)*10;
-
-        cairo_set_source_rgba (cr_image, 1.0/(i+1), 1.0, 16.0*i/255.0, 1.0);
-        cairo_rectangle (cr_image, x, y, 10, 10);
-        cairo_fill (cr_image);
-    }
-
-    cairo_destroy (cr_image);
-
+    image = cairoCreateSampleImage (ctx);
     return 1;
 }
 
